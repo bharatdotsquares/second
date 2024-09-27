f827720a8da17b44166beb0659ad32b946fe4880
Bharat Singh <bharatdotsquares@gmail.com>
2024-09-27 18:44:54 +0530

    auth by mongodb

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { CredentialsSignin } from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import GithubProvider from "next-auth/providers/github"
 import { authConfig } from '../auth.config';
@@ -6,18 +6,18 @@ import {z} from "zod"
 import { AdapterUser } from 'next-auth/adapters';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import client from '@/lib/db';
+import { connectDB } from "@/lib/mongodb";
+import User from "@/models/User";  
+import bcrypt from "bcryptjs";
 
-
-interface User{
-  name:string;
-  email:string;
-  image:string;
-  emailVerified:string;
+interface User{ 
+  email:string; 
+  password:string;
 }
 
 export const {handlers, auth, signIn, signOut } = NextAuth({
   ...authConfig,
-  adapter: MongoDBAdapter(client),
+  // adapter: MongoDBAdapter(client),
   providers: [
     Credentials({
         credentials: {
@@ -35,14 +35,46 @@ export const {handlers, auth, signIn, signOut } = NextAuth({
         authorize:async (credentials)=> { // you have access to the original request as well
             // console.log(request)
             const parsedCredentials = z
-            .object({ email: z.string().email(), password: z.string().min(6) })
+            .object({name: z.string(),email: z.string().email(), password: z.string().min(6) })
             .safeParse(credentials);
+         
+            if (!parsedCredentials.success) {
+              return {
+                  errors: parsedCredentials.error.flatten().fieldErrors,
+                  message: 'Missing Fields. Failed to Create Invoice.',
+              };
+            }
 
-            
-            if(parsedCredentials.success) return {username:"hello",password:"world",name:"hello world"}
+            await connectDB();
+            const user = await User.findOne({
+              email: credentials?.email,
+            }).select("+password");
+ 
+            if (!user){
+              return {
+                errors: [{email:"wrong email"}],
+                message: 'Wrong Email',
+              };
+            }
 
-            return null;
-            // return await getUser(credentials) // assuming it returns a User or null
+            const passwordMatch = await bcrypt.compare(
+              credentials!.password,
+              user.password
+            );
+
+            if (!passwordMatch){
+              return  {
+                errors: [{password:"wrong password"}],
+                message: 'Wrong password',
+              };
+            };
+            let obj = {email:user.email,id:user._id}
+            console.log(obj)
+            return obj;
+
+            if(!credentials.email){
+              throw new CredentialsSignin({cause:"Email is required"})
+            }
           } 
     }),
     GithubProvider({
@@ -63,3 +95,4 @@ export const {handlers, auth, signIn, signOut } = NextAuth({
     },
   },
 });
+ 
